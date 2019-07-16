fe162443b35396c6ef974577f5c1714eda1fe7ab
davisc0801 <davisc1014@gmail.com>
2019-07-16 17:54:13 -0600

    Add feature test for splash page

---
+++ spec/features/welcome_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+describe "as a visitor when I visit /" do
+  it "shows a splash page with links" do
+    visit root_path
+    expect(page).to have_link("Login")
+    expect(page).to have_link("Register")
+    expect(page).to have_link("About Us")
+  end
+end
