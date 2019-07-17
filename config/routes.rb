837a7557b73bf2e6757f423158a2fb1da6d69b0b
davisc0801 <davisc1014@gmail.com>
2019-07-16 18:20:12 -0600

    Add routes needed for splash page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'welcome#index'
+  get "/about", to: 'welcome#show', as: "about"
+  get "logout", to: "sessions#destroy", as: "logout"
+  get "login", to: "sessions#new", as: "login"
+  get "register", to: "users#new", as: "register"
 end
