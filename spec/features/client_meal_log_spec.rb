0c0564cdefc02f252bd4e38881c297b5ebe074c7
davisc0801 <davisc1014@gmail.com>
2019-07-18 19:17:08 -0600

    Change tests to use form_tag syntax instead

--- spec/features/client_meal_log_spec.rb
+++ spec/features/client_meal_log_spec.rb
@@ -10,8 +10,7 @@ describe "As a client when I visit my dashboard" do
     visit dashboard_path
     click_link("Log a Meal")
     expect(current_path).to eq(new_meal_log_path)
-    fill_in("meal[name_search]", with: "pizza")
+    fill_in("meal_input", with: "pizza")
     click_button("Large")
-    expect(current_path).to eq(new_meal_log_path)
   end
 end
