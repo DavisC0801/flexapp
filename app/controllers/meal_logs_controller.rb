89f442b26e659077a335ae5c037487ef553e7881
davisc0801 <davisc1014@gmail.com>
2019-07-19 17:59:59 -0600

    Add create action to the meal log controller

--- app/controllers/meal_logs_controller.rb
+++ app/controllers/meal_logs_controller.rb
@@ -5,6 +5,17 @@ class MealLogsController < ApplicationController
     }
   end
 
+  def create
+    #todo - Update to scope to current_user
+    meal = MealLog.new(meal_log_params)
+    if meal.save
+      redirect_to dashboard_path
+    else
+      flash.now[:danger] = meal.errors.full_messages
+      render :new
+    end
+  end
+
   private
 
   def meal_log_params
