16c4c2f7f697ce581a142700b7419ff516cd0f34
davisc0801 <davisc1014@gmail.com>
2019-07-18 18:30:56 -0600

    Add meal log new page to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'welcome#index'
   get "/about", to: 'welcome#show'
-  
+
   get "/logout", to: "sessions#destroy"
   get "/login", to: "sessions#new"
 
   get "/register", to: "users#new"
   get "/dashboard", to: "users#show"
+
+  resource :meal_log, only: :new
 end
