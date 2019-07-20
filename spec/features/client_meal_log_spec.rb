637de0a7bbd3d9b962eb16d7baca1a997a148eed
davisc0801 <davisc1014@gmail.com>
2019-07-20 12:42:29 -0600

    Add sad path and manual logging specs

--- spec/features/client_meal_log_spec.rb
+++ spec/features/client_meal_log_spec.rb
@@ -18,13 +18,13 @@ describe "As a client when I visit my dashboard" do
     end
   end
 
-  it "creates a meal log" do
+  it "creates a meal log using the API" do
     VCR.use_cassette("meal/pizza") do
       visit new_meal_logs_path
       fill_in("meal_input", with: "pizza")
       click_button("Large")
       click_button("Log Meal")
-      
+
       test_log = MealLog.last
       expect(test_log.name).to eq("pizza")
       expect(test_log.meal_calories).to eq(284)
@@ -34,5 +34,35 @@ describe "As a client when I visit my dashboard" do
       expect(test_log.meal_protein).to eq(12)
       expect(test_log.meal_sodium).to eq(639)
     end
+  end
+
+  it "creates a meal log manually" do
+    visit new_meal_logs_path
+    fill_in("meal_log[name]", with: "pizza")
+    fill_in("meal_log[meal_calories]", with: "1")
+    fill_in("meal_log[meal_fats]", with: "2")
+    fill_in("meal_log[meal_carbs]", with: "3")
+    fill_in("meal_log[meal_sugars]", with: "4")
+    fill_in("meal_log[meal_protein]", with: "5")
+    fill_in("meal_log[meal_sodium]", with: "6")
+    click_button("Log Meal")
+
+    test_log = MealLog.last
+    expect(test_log.name).to eq("pizza")
+    expect(test_log.meal_calories).to eq(1)
+    expect(test_log.meal_fats).to eq(2)
+    expect(test_log.meal_carbs).to eq(3)
+    expect(test_log.meal_sugars).to eq(4)
+    expect(test_log.meal_protein).to eq(5)
+    expect(test_log.meal_sodium).to eq(6)
+  end
+
+  it "does not create a meal with missing information" do
+    visit new_meal_logs_path
+    click_button("Log Meal")
+
+    test_log = MealLog.last
+    expect(current_path).to eq(new_meal_logs_path)
+    expect(test_log).to be nil
   end
 end
