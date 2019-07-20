11a8de1d4f1f389264a29678fd614e1b9e460083
Trevor Nodland <trevor.nodland@gmail.com>
2019-07-20 14:15:32 -0600

    begins workin on email, need to commit to switch branches

---
+++ spec/features/users/client_can_be_invited_by_trainer_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe "email invitation" do
+  context "as a trainer" do
+    it "can send an email for a new client to create an account" do
+
+    end
+  end
+end
