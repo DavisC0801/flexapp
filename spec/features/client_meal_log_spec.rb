d6c6421e650e60c5c44cad84f0123c70226d2273
davisc0801 <davisc1014@gmail.com>
2019-07-20 12:28:02 -0600

    Add spec for creating a meal log using lookup

--- spec/features/client_meal_log_spec.rb
+++ spec/features/client_meal_log_spec.rb
@@ -7,10 +7,32 @@ describe "As a client when I visit my dashboard" do
   end
 
   it "It links to a place to enter a meal name and portion size" do
-    visit dashboard_path
-    click_link("Log a Meal")
-    expect(current_path).to eq(new_meal_log_path)
-    fill_in("meal_input", with: "pizza")
-    click_button("Large")
+    VCR.use_cassette("meal/pizza") do
+      visit dashboard_path
+      click_link("Log a Meal")
+      expect(current_path).to eq(new_meal_logs_path)
+      fill_in("meal_input", with: "pizza")
+      click_button("Large")
+      expect(current_path).to eq(new_meal_logs_path)
+      # expect(page).to have_css("value=284")
+    end
+  end
+
+  it "creates a meal log" do
+    VCR.use_cassette("meal/pizza") do
+      visit new_meal_logs_path
+      fill_in("meal_input", with: "pizza")
+      click_button("Large")
+      click_button("Log Meal")
+      
+      test_log = MealLog.last
+      expect(test_log.name).to eq("pizza")
+      expect(test_log.meal_calories).to eq(284)
+      expect(test_log.meal_fats).to eq(10)
+      expect(test_log.meal_carbs).to eq(35)
+      expect(test_log.meal_sugars).to eq(7)
+      expect(test_log.meal_protein).to eq(12)
+      expect(test_log.meal_sodium).to eq(639)
+    end
   end
 end
