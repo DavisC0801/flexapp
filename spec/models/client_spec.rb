f291c681f59c154f7f15d5a9e6885a2209c7c0b4
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-07-19 19:13:13 -0600

    Add trainer and client model tests

---
+++ spec/models/client_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Client, type: :model do
+  describe "relationships" do
+    it {should belong_to :trainer}
+  end
+
+  describe "validations" do
+    it { should validate_presence_of :email }
+    it { should validate_uniqueness_of :email }
+    it { should validate_presence_of :first_name }
+    it { should validate_presence_of :last_name }
+    it { should validate_presence_of :password }
+    it { should validate_presence_of :phone_num }
+  end
+
+end
