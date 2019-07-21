77ecef034d27e81653cda9f9c3689547068e2b31
Vince Carollo <vincecarollo@gmail.com>
2019-07-21 16:20:29 -0600

    Fix wording in client log reminder spec

--- spec/features/clients/clients_are_reminded_to_weigh_in_spec.rb
+++ spec/features/clients/clients_are_reminded_to_weigh_in_spec.rb
@@ -2,28 +2,25 @@ require 'rails_helper'
 
 RSpec.describe "As a registered client" do
   describe 'on my dashboard' do
-    describe 'if my last weigh in log is over a week' do
+    before :each do
+      @client = create(:client)
+      allow_any_instance_of(ApplicationController).to receive(:current_client).and_return(@client)
+    end
 
-      before :each do
-        @client = create(:client)
-        allow_any_instance_of(ApplicationController).to receive(:current_client).and_return(@client)
-      end
+    it "prompts for a weigh in if last log is week old" do
+      log1 = @client.weight_logs.create(weight: 123, created_at: Date.today - 8)
 
-      it "prompts for a weigh in if last log is week old" do
-        log1 = @client.weight_logs.create(weight: 123, created_at: Date.today - 8)
+      visit client_dashboard_path
+      expect(page).to have_content("It has been over a week since you've logged your weight")
+      expect(page).to have_link('Log Weight')
+    end
 
-        visit client_dashboard_path
-        expect(page).to have_content("It has been over a week since you've logged your weight")
-        expect(page).to have_link('Log Weight')
-      end
+    it "doesn't prompt for a weigh if log is current" do
+      log1 = @client.weight_logs.create(weight: 123)
 
-      it "doesn't prompt for a weigh if log is current" do
-        log1 = @client.weight_logs.create(weight: 123)
-
-        visit client_dashboard_path
-        expect(page).to_not have_content("It has been over a week since you've logged your weight")
-        expect(page).to have_link('Log Weight')
-      end
+      visit client_dashboard_path
+      expect(page).to_not have_content("It has been over a week since you've logged your weight")
+      expect(page).to have_link('Log Weight')
     end
   end
 end
