fda554092914312536db043816837cf0ec8b94fb
Vince Carollo <vincecarollo@gmail.com>
2019-07-20 19:25:33 -0600

    Add flash notices to application.html

--- app/controllers/clients/trainer_messages_controller.rb
+++ app/controllers/clients/trainer_messages_controller.rb
@@ -5,5 +5,7 @@ class Clients::TrainerMessagesController < Clients::BaseController
     message = params[:message]
     send_number = "+1" + current_client.trainer.phone_num
     TwilioTextMessenger.new(message, send_number).call
+    flash.notice = "Successfully Sent Message"
+    redirect_to client_dashboard_path
   end
 end
