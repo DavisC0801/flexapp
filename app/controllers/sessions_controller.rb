1bd117b6bf5f7ea989bf92fa940f081a3ced931e
Trevor Nodland <trevor.nodland@gmail.com>
2019-07-22 11:16:32 -0600

    adds functionality for a trainer to log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,12 @@ class SessionsController < ApplicationController
         session[:client_id] = client.id
         redirect_to client_dashboard_path
       end
+    elsif Trainer.find_by(email: params[:email])
+      trainer = Trainer.find_by(email: params[:email])
+      if trainer&.authenticate(params[:password])
+        session[:trainer_id] = trainer.id
+        redirect_to trainer_dashboard_path
+      end
     end
   end
 end
--- spec/features/users/user_can_log_in_and_out_spec.rb
+++ spec/features/users/user_can_log_in_and_out_spec.rb
@@ -15,4 +15,19 @@ RSpec.describe "sessions spec" do
       expect(page).to have_current_path(client_dashboard_path)
     end
   end
+
+  context "as a trainer" do
+    it "can log in" do
+      trainer = create(:trainer)
+
+      visit '/'
+
+      fill_in 'Email', with: trainer.email
+      fill_in 'Password', with: trainer.password
+
+      click_on 'Log In'
+
+      expect(page).to have_current_path(trainer_dashboard_path)
+    end
+  end
 end
