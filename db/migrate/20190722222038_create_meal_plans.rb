e75e219e69a397b598a7302871b4209d86c657f9
Vince Carollo <vincecarollo@gmail.com>
2019-07-22 16:28:37 -0600

    Add meal plan table to db

---
+++ db/migrate/20190722222038_create_meal_plans.rb
@@ -0,0 +1,14 @@
+class CreateMealPlans < ActiveRecord::Migration[5.2]
+  def change
+    create_table :meal_plans do |t|
+      t.integer :calories
+      t.boolean :vegetarian, :default => false
+      t.boolean :vegan, :default => false
+      t.string :diet_type
+      t.string :excluded
+      t.references :client, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
