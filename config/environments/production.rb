55e166390dfb73836ba9096a25c0077b85345520
Trevor Nodland <trevor.nodland@gmail.com>
2019-07-22 11:26:42 -0600

    sets up email for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => "https://brownfield-of-sorrow.herokuapp.com"}
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.sendgrid.net',
+    port:                 '587',
+    domain:               'flexapp1903.herokuapp.com',
+    user_name:            ENV["SENDGRID_USERNAME"],
+    password:             ENV["SENDGRID_PASSWORD"],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 end
