c80937ce80f2b0f679ed284dcbd16fd7d66d9fc8
Vince Carollo <vincecarollo@gmail.com>
2019-07-23 14:55:36 -0600

    Chnage string sub to global sub for multiple spaced meal names

--- app/services/food_search_service.rb
+++ app/services/food_search_service.rb
@@ -1,10 +1,7 @@
 class FoodSearchService
-  def connection
-    Faraday.new("https://thawing-lowlands-89167.herokuapp.com")
-  end
 
   def food_lookup(search_terms)
-    food = search_terms[:meal_name].downcase.sub(" ", "%20")
+    food = search_terms[:meal_name].downcase.gsub(" ", "%20")
     size = search_terms[:size].downcase
     response = connection.get("/food_info/#{food}/", {size: size})
     data = JSON.parse(response.body, symbolize_names: true)
@@ -19,5 +16,11 @@ class FoodSearchService
     end
     response = connection.get("/meals?calories=#{meal_plan.calories}&restriction=#{restriction}&diet_type=#{meal_plan.diet_type}&excluded=#{meal_plan.excluded}")
     JSON.parse(response.body, symbolize_names: true)
+  end
+
+  private
+
+  def connection
+    Faraday.new("https://thawing-lowlands-89167.herokuapp.com")
   end
 end
