f9cae5e534c5a32b1663dc65c7149118787f8a97
Vince Carollo <vincecarollo@gmail.com>
2019-07-23 16:06:58 -0600

    Add sample client seed data and add bootstrap styling to recipe show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+client = Client.create!(first_name: 'Jon', last_name: 'Fav', email: 'vincecarollo@gmail.com', phone_num: '3034351644', password: 'password')
+client.create_meal_plan(calories: 200, vegetarian: true, diet_type: 'high-protein')
