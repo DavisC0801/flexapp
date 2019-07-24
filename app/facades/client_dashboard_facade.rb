7ef977faeabc5a650c5f65c7e49017f2aae17a64
Vince Carollo <vincecarollo@gmail.com>
2019-07-24 14:54:45 -0600

    Finish mealtracker graph on client dashboard

--- app/facades/client_dashboard_facade.rb
+++ app/facades/client_dashboard_facade.rb
@@ -3,9 +3,38 @@ class ClientDashboardFacade
     @client = client
   end
 
+  def meals
+    @meals ||= @client.meal_logs.take(5)
+  end
+
   def meal_names
-    @client.meal_logs
-    binding.pry
+    meals.map do |meal|
+      meal.name.capitalize
+    end
+  end
+
+  def meal_carbs
+    meals.map do |meal|
+      meal.meal_carbs
+    end
+  end
+
+  def meal_fats
+    meals.map do |meal|
+      meal.meal_fats
+    end
+  end
+
+  def meal_sugars
+    meals.map do |meal|
+      meal.meal_sugars
+    end
+  end
+
+  def meal_proteins
+    meals.map do |meal|
+      meal.meal_protein
+    end
   end
 
 end
