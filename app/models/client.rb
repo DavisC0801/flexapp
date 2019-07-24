d94c65a88ab676728df349f57846e97a06aa5b73
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-07-24 10:24:47 -0600

    Add progress method to client model with temp functionality

--- app/models/client.rb
+++ app/models/client.rb
@@ -5,4 +5,10 @@ class Client < ApplicationRecord
   validates :email, uniqueness: true, presence: true
   validates_presence_of :first_name, :last_name, :password
   has_secure_password
+
+  def progress
+    p = (55.to_f/100)
+    p = (p * 100).round
+    q = "#{p}%"
+  end
 end
