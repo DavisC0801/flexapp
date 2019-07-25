fc6640864c408cb528cf31bae525c2bb7a1e0fcc
Vince Carollo <vincecarollo@gmail.com>
2019-07-25 10:54:25 -0600

    Change meal tracker graph to display nutrients by DAY
    Co-Authored-By: Trevor Nodland <42710030+tnodland@users.noreply.github.com>

--- app/models/meal_log.rb
+++ app/models/meal_log.rb
@@ -4,4 +4,14 @@ class MealLog < ApplicationRecord
   belongs_to :client
 
   default_scope { order(meal_date: :desc) }
+
+  def self.meals_this_week
+    where(meal_date: [(Date.today - 7)..Date.today])
+        .select('SUM(meal_logs.meal_fats) AS total_fats')
+        .select('SUM(meal_logs.meal_sugars) AS total_sugars')
+        .select('SUM(meal_logs.meal_protein) AS total_protein')
+        .select('SUM(meal_logs.meal_carbs) AS total_carbs')
+        .select(:meal_date)
+        .group(:meal_date)
+  end
 end
