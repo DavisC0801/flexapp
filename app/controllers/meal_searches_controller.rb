eb420393adec564cb5f6055e10c68077c8b31e51
davisc0801 <davisc1014@gmail.com>
2019-07-25 11:10:30 -0600

    Add additional sad path validation and feedback to meal searches and food logs

--- app/controllers/meal_searches_controller.rb
+++ app/controllers/meal_searches_controller.rb
@@ -2,6 +2,11 @@ class MealSearchesController < ApplicationController
   def create
     search_terms = {meal_name: params[:meal_input], size: params[:commit]}
     search = FoodSearchService.new
-    redirect_to new_meal_logs_path(search.food_lookup(search_terms))
+    if search_terms[:meal_name].empty? || search_terms[:meal_name].nil?
+      flash[:error] = "Invalid Meal Name, Please Try Again."
+      redirect_to new_meal_logs_path
+    else
+      redirect_to new_meal_logs_path(search.food_lookup(search_terms))
+    end
   end
 end
